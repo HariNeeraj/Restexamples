36350bb3c2e425467db51cddbbb264514789cd0b
HariNeeraj <harineeraj.king@gmail.com>
2022-11-04 17:59:40 +0530

    teste change

--- src/test/java/Practice/CookieandHeadersTest.java
+++ src/test/java/Practice/CookieandHeadersTest.java
@@ -1,16 +1,14 @@
 package Practice;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.Map;
+
 import org.testng.annotations.Test;
 
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-
-import java.util.Map;
 
 public class CookieandHeadersTest {
 	
